ee08261c64712c53f3076a3e6530bded80d3ef8b
nabinnath9 <nabinnath9@gmail.com>
2021-02-06 20:45:07 +0530

    Registration Page added

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:covid_app/register.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Register(),
+      ),
+    );
+  }
+}
